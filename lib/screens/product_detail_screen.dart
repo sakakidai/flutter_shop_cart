f3b18301b6d1afd2a3ea76bba2ea576ea8e9e539
sakaki <sakaki@samubuc.co.jp>
2022-04-30 09:26:28 +0900

    Fixed product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,7 +16,29 @@ class ProductDetailScreen extends StatelessWidget {
     ).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title)),
-    );
+        appBar: AppBar(title: Text(loadedProduct.title)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+              ),
+              const SizedBox(height: 10),
+              Text('\$${loadedProduct.price}', style: const TextStyle(color: Colors.grey, fontSize: 20)),
+              const SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
